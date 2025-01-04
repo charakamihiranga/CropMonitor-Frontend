d2a870c3267e45878f3d1e089f4ccd499881ad56
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2025-01-04 21:10:09 +0530

    Add account deletion functionality with confirmation modal

--- model/UserModel.js
+++ model/UserModel.js
@@ -32,3 +32,30 @@ export function changeUserPassword(userEmail, user) {
       return { status: error.status, message: errorMessage };
     });
 }
+
+export function removeUser(userEmail) {
+  const token = getJwtTokenFromCookies();
+  if (!token) {
+    console.error("JWT token not found in cookies.");
+    return;
+  }
+  return $.ajax({
+    url: `${API_URL}/user/${userEmail}`,
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return { status: 204, message: "User deleted successfully" };
+    })
+    .catch((error) => {
+      let errorMessage = "An error occurred while deleting user. Please try again.";
+      if (error.status === 404) {
+        errorMessage = "User not found. Please check the provided useremail.";
+      } else if (error.responseJSON && error.responseJSON.message) {
+        errorMessage = error.responseJSON.message;
+      }
+      return { status: error.status, message: errorMessage };
+    });
+}
